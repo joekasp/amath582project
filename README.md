9b8c6621c5320b197b1a39144398d11878920d35
Joe Kasper <jkasper2@uw.edu>
2016-03-08 10:16:04 -0800

    Updated the compressed_sensing.m file with an idealized case. Note that you'll need to install the cvx package to run it.

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Use git clone to create a local copy on your machine.
 Use git add to add a new file that you created so git will track it.
 Use git commit -a to save a snapshot of the local changes (along with a comment/message about what you did).
 Use git push to send the changes to the github repository.
-Use git fetch --all or git update to get the most recent edition of the entire repo.
+Use git pull origin to get the most recent edition of the entire repo.
 
--- compressed_sensing.m
+++ compressed_sensing.m
@@ -2,6 +2,33 @@
 %AMATH 582 Final Project
 %J. Kasper, C. Houferak, J. Radler, S. Sun
 
-%% Step 1:
+%% Step 1: Known Signal
+t = linspace(0,1,1000);
+S = sin(3*t)+sin(10*t)+sin(40*t); %idealized signal
+
+plot(t,S)
+xlabel('Time')
+ylabel('Signal Amplitude')
+title('Idealized Signal')
+
+n=50; %number of points to sample
+samp = S(1:n);
+
+deltaOmega = 1; %frequency difference
+
+%generate Fourier matrix
+for j = 1:3
+    for k = 1:1000
+        F(j,k) = (2/pi)*deltaOmega*sin(t(j)*t(k));
+    end
+end
+
+cvx_begin;
+    variable rec(t);
+    minimize(norm(rec,1));
+    subject to
+    F*rec == samp;
+
+cvx_end;
 
 %% Step 2:
